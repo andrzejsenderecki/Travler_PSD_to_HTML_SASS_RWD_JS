f74c87b253ec41a1489aa8b6f3a29e6726d85903
Andrzej Senderecki <skidrzej@gmail.com>
2018-12-07 23:12:06 +0100

    Slider - rozpoczęcie pracy

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,11 @@
 require('../scss/modules/_variables.scss');
 require('../scss/partials/_main.scss');
 require('../scss/partials/_header.scss');
+require('../scss/partials/_slider.scss');
 
 document.addEventListener("DOMContentLoaded", function () {
+
+    // Responsive menu
 
     let menuBtnResponsive = document.createElement('div');
     menuBtnResponsive.classList.add('responsive-menu-btn');
@@ -13,24 +16,72 @@ document.addEventListener("DOMContentLoaded", function () {
     let menuSection = document.querySelector('.navbar-menu');
     let logoSection = document.querySelector('.navbar-logo');
 
-    let mobile = window.matchMedia("(min-width: 822px)")
+    let mobile = window.matchMedia("(min-width: 822px)");
 
-        let checkMobile = () => {
-            if (mobile.matches) {
-                menuSection.classList.remove('displayToggle');
-                let toRemove = document.querySelector('.responsive-menu-btn');
-                if(toRemove !== null) {
-                    toRemove.parentElement.removeChild(toRemove);
-                }
-            } else  {
-                menuSection.classList.add('displayToggle');
-                logoSection.appendChild(menuBtnResponsive);
+    let checkMobile = () => {
+        if (mobile.matches) {
+            menuSection.classList.remove('displayToggle');
+            let toRemove = document.querySelector('.responsive-menu-btn');
+            if(toRemove !== null) {
+                toRemove.parentElement.removeChild(toRemove);
             }
+        } else  {
+            menuSection.classList.add('displayToggle');
+            logoSection.appendChild(menuBtnResponsive);
+        }
+    }
+
+    checkMobile();
+
+    mobile.addListener( function(mobile) {
+        checkMobile();
+    });
+
+    //Slider
+
+    let slider = document.querySelectorAll('.slider li');
+    let btnNext = document.querySelector('.nextSlide');
+    console.log(btnNext);
+    let btnPrev = document.querySelector('.prevSlide');
+
+    let slideCounter = 0;
+
+    let slideTime = setInterval(() => {
+        if(slideCounter<slider.length) {
+            slideCounter += 1;
+        } else {
+            slideCounter = 0;
         }
 
-        checkMobile();
+        checkSlideCounter();
+    }, 3000);
 
-        mobile.addListener( function(mobile) {
-            checkMobile();
-        });
+    let checkSlideCounter = () => {
+        for(let i=1; i<slider.length; i++) {
+            if(slideCounter === i) {
+                slider[i].classList.add('sliderVisible');
+                slider[i].classList.remove('sliderInvisible');
+            } else {
+                slider[i].classList.add('sliderInvisible');
+                slider[i].classList.remove('sliderVisible');
+            }
+        }
+    }
+    console.log(slider.length);
+    btnNext.addEventListener('click', () => {
+        if(slideCounter < slider.length-1) {
+            slideCounter += 1;
+        }
+        console.log(slideCounter);
+        checkSlideCounter();
+    });
+
+    btnPrev.addEventListener('click', () => {
+        if(slideCounter > 0) {
+            slideCounter -= 1;
+        }
+        console.log(slideCounter);
+        checkSlideCounter();
+    });
+
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,35 @@
 module.exports={
-    mode: "development",
-    entry:"./scripts/app.js",
-    watch: true,
-    output: {
-        filename: "../scripts/out.js"
-    },
-    module: {
-        rules: [
-
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.css/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            ['env', {
-                                targets: {
-                                    browsers: [
-                                        'Chrome 49'
-                                    ]
-                                }
-                            }]
-                        ]
-                    }
-                }
-            }]
-    }
+   mode: "development",
+   entry:"./scripts/app.js",
+   watch: true,
+   output: {
+       filename: "../scripts/out.js"
+   },
+   module: {
+       rules: [
+           {
+               test: /\.scss$/,
+               use: ['style-loader', 'css-loader?url=false',
+                   'sass-loader',
+               ],
+           },
+           {
+               test: /\.js$/,
+               exclude: /node_modules/,
+               use: {
+                   loader: 'babel-loader',
+                   options: {
+                       presets: [
+                           ['env', {
+                               targets: {
+                                   browsers: [
+                                       'Chrome 49'
+                                   ]
+                               }
+                           }]
+                       ]
+                   }
+               }
+           }]
+   }
 };
